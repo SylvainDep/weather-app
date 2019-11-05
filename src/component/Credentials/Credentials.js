f4db90744f0b017cf22fc7a78434889be265aa5a
Sylvain Depardieu <depardieu@sendesignal.de>
2019-11-05 10:24:11 +0100

    Added Github link

--- src/component/Credentials/Credentials.js
+++ src/component/Credentials/Credentials.js
@@ -6,7 +6,7 @@ const Credentials = (props) => {
   return (
     <div className={classes.Credentials}>
       <p>Developped with <img className={classes.triskel} src="http://corinne-fablet-therapeute.com/images/triskel_blanc.png?crc=3807833994" /> by Sylvain Depardieu</p>
-      <p>View Code on Github</p>
+      <p>View Code on <a href="https://github.com/SylvainDep/weather-app">Github</a></p>
     </div>
   )
 }
